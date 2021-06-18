936646012918064266757a6015f07a254bff55b3
ricardo85x <ricardo85x@gmail.com>
2021-06-18 19:49:40 +0000

    Deploying to gh-pages from @ ricardo85x/tokenize-dapp@4b85eb558be25ecea80565d009f8a52805b00cce 🚀

--- _next/static/jhPZvzgrDHbJUipE2-_KU/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/943-9d57e7a798e093c66eef.js","static/chunks/pages/index-7609711fc741a876db4e.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
