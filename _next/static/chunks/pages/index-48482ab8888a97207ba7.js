4ac4c5c8223c3922914a1b63308209db0a06a164
ricardo85x <ricardo85x@gmail.com>
2021-07-01 23:55:56 +0000

    Deploying to gh-pages from @ ricardo85x/tokenize-dapp@d1e0359e0bda313e8eae95a191bb795ff6ba0ffe 🚀

---
+++ _next/static/chunks/pages/index-48482ab8888a97207ba7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{6839:function(n,u,t){"use strict";t.r(u);var _=t(5893),r=t(3188);u.default=function(){return(0,_.jsx)(r.O,{})}},5301:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return t(6839)}])}},function(n){n.O(0,[579,188,774,888,179],(function(){return u=5301,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
